198be23627f57052be2101761577de5318f5e5a0
tnichlani <tnichlani@gmail.com>
2023-01-05 22:44:43 -0500

    react 2 one

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import './Test.css'
+
+function Test(){
+    return(
+        <div>
+            <p className="greeting">
+                Bye
+            </p>
+        </div>
+    )
+}
+
+export default Test
